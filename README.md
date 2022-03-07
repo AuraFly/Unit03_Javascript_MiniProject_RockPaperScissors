bddd6c81f8d02f850f1e09c10a49c7baf416159b
AuraFly <JordanJCo@gmail.com>
2022-03-07 12:31:37 -0700

    Finished it - thanks for the video Jay!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Unit03_Javascript_MiniProject_RockPaperScissors
 Building a rock paper scissors game via javascript
+
+Followed along with Jay's video after a few attempts of trying on my own. Couldnt get it working, this looks very similar to Jay's but I added a few things, and used several else statements instead of the || option just because that is what I had in my logic before Jay went over things.
+
+Thanks Jay!
--- script.js
+++ script.js
@@ -2,38 +2,59 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
-function userInput () {
-var userChoices = ["Rock", "Paper", "Scissors"];
+var options = ["Rock", "Paper", "Scissors"]
+
+function playGame() {
+    var userChoice = window.prompt("Enter your selection - Rock, Paper, or Scissors:");
+    if (!userChoice) { 
+        alert("Thanks for playing!")
+    return;
+    }
+
+    userChoice = userChoice.toUpperCase();
+
+    var initCPUChoice = Math.floor(Math.random()*options.length);
+    var cpuChoice = options[initCPUChoice];
+
+    cpuChoice = cpuChoice.toUpperCase();
+
+    window.alert("The computer's choice is " + cpuChoice);
+
+    if (userChoice === cpuChoice) {
+        window.alert("Its a tie! Ill beat you next time");
+        ties++;
+    } else if (userChoice === "ROCK" && cpuChoice === "PAPER") {
+        window.alert("I beat you. Of course, you are only human and I am a great computer!");
+        losses++;
+    } else if (userChoice === "ROCK" && cpuChoice === "SCISSORS") {
+        window.alert("You won! Impressive for a human!");
+        wins++;
+    } else if (userChoice === "SCISSORS" && cpuChoice === "ROCK") {
+        window.alert("I beat you. Of course, you are only human and I am a great computer!");
+        losses++;
+    } else if (userChoice === "SCISSORS" && cpuChoice === "PAPER") {
+        window.alert("You won! Impressive for a human!");
+        wins++;
+    } else if (userChoice === "PAPER" && cpuChoice === "SCISSORS") {
+        window.alert("I beat you. Of course, you are only human and I am a great computer!");
+        losses++;
+    } else if (userChoice === "PAPER" && cpuChoice === "ROCK") {
+        window.alert("You won! Impressive for a human!")
+        wins++;
+    }
+
+    window.alert("Here are your stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    var playAgain = window.confirm("Play again?")
+
+    if (playAgain) {
+        playGame();}
+        else {
+            window.alert("Thanks for playing!")
+            return;
+    }
+
 }
 
-function comp () {
-    var computerChoice = userChoices[Math.floor(Math.random()*userChoices.length)];
-    return option
-}
-function choiceComparison(x,y) {
-    x = userChoices;
-    y = computerChoices;
-    if (x === y) {
-        alert("Its a tie! Ill beat you next time");
-        console.log(++ties);
-    } else if ( x === userChoices[0] && y === computerChoices) {
-        alert("I beat you. Of course, you are only human and I am a great computer!");
-        console.log(++losses);
-    } else if (x === userChoices[0] && y === choices[2]) {
-        alert("You won! Impressive for a human!");
-        console.log(++wins);
-    } else if ( x === userChoices[1] && y === choices[2]) {
-        alert("I beat you. Of course, you are only human and I am a great computer!");
-        console.log(++losses);
-    } else if (x === userChoices[1] && y === choices[0]) {
-        alert("You won! Impressive for a human!");
-        console.log(++wins);
-    } else if ( x === userChoices[2] && y === choices[0]) {
-        alert("I beat you. Of course, you are only human and I am a great computer!");
-        console.log(++losses);
-    } else if (x === userChoices[2] && y === choices[1]) {
-        alert("You won! Impressive for a human!")
-        console.log(++wins);
-})
+playGame();
 
-prompt("Type Rock, Paper, or Scissors")
